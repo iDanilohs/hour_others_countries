7c3ac6cc65d270395a395a35f7d415b8242e5ee3
Danilo Herrera <daniloherrera1702@gmail.com>
2022-12-01 00:37:08 -0500

    The basic structure of the algorimth; but we have a problem with the format of dates

--- code_hours_in_others_countries.py
+++ code_hours_in_others_countries.py
@@ -1,5 +1,13 @@
+def tell_me_the_hour(country, hour):
+    if country == 1:
+        return print(hour - 1)
+    elif country == 2:
+        return print(hour + 1)
+
 def run():
-    pass
+    country = int(input('México to Colmbia = 1 or Colombia to México = 2: '))
+    hour_in_country_1 = int(input('Put the hour in your country: '))
+    print(tell_me_the_hour(country, hour_in_country_1))
 
 if __name__ == '__main__':
     run()
